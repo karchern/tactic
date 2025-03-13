0727b1e54d94ffb973a2965c34930ddb6f19f686
Nicolai Karcher <karchernic@gmail.com>
2025-03-13 08:47:58 +0100

    slightly guess-dapting fix_maps.r (DISCUSS WITH ALESSIO) running fix_maps.r to update  input maps in maps folder

--- fix_maps.r
+++ fix_maps.r
@@ -5,6 +5,7 @@ files_modified <- map(files, \(x) {
     plate_number <- str_split(x, "_plate_")[[1]][2]
     plate_number <- str_replace(plate_number, ".csv", "")
     plate_number <- as.numeric(plate_number)
+    # TODO: Show this below to Alessio (mention the change you did for screen 1), make sure it's sensible (I can't quite remember why I needed to do this)
     if (
         plate_number %in% c(1, 2, 3)
     ) {
@@ -15,6 +16,11 @@ files_modified <- map(files, \(x) {
     ) {
         xx <- read_csv(x) %>%
             mutate(biorep96 = 2)
+    } else if (
+        plate_number %in% c(7, 8, 9)
+    ) {
+        xx <- read_csv(x) %>%
+            mutate(biorep96 = 3)
     } else {
         stop("This should never be reached - check your maps files for consistency")
     }
@@ -23,6 +29,6 @@ files_modified <- map(files, \(x) {
         mutate(plt96 = plate_number)
     return(xx)
 })
-map2(files, files_modified, \(x, y) {
+walk2(files, files_modified, \(x, y) {
     write_csv(y, x)
 })
